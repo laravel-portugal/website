14734fece37e617893055c59b169ae9749db2d9d
Tiago Fernandes <tiagoff@gmail.com>
2021-05-05 09:03:26 +0100

    layout usage fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,9 @@
         <x-jet-banner />
 
         <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+            @auth
+                @livewire('navigation-menu')
+            @endauth
 
             <!-- Page Heading -->
             @if (isset($header))
