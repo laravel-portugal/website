7adf925409311245fb8282d2385992284d756859
Tiago Fernandes <tiagoff@gmail.com>
2021-05-05 17:38:38 +0100

    .

--- domains/Discussions/Resources/Views/questions-index.blade.php
+++ domains/Discussions/Resources/Views/questions-index.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="max-w-screen-xl mx-auto">
+    <div class="max-w-screen-xl mx-auto px-4 sm:px-8">
         <div class="text-4xl">Discussions</div>
         <div class="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet beatae debitis delectus, deleniti dolorem eum exercitationem expedita fuga inventore nemo officia, officiis quae reiciendis rem veniam. Dolore non odio sapiente.</div>
 
