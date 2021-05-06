1167a4a5097b49a07a67bdaa466b9ff1753afc40
Tiago Fernandes <tiagoff@gmail.com>
2021-05-06 14:39:25 +0100

    .

--- domains/Links/Resources/Views/recent-links.blade.php
+++ domains/Links/Resources/Views/recent-links.blade.php
@@ -8,7 +8,7 @@
         As dicas mais inovadoras, criadas pela comunidade, para a comunidade.
     </x-hero>
 
-    <div class="relative text-center mx-auto max-w-screen-2xl mt-10 px-2 sm:mt-12 sm:px-4 md:mt-24 md:px-8 lg:mt-32">
+    <div class="relative mx-auto max-w-screen-2xl mt-10 px-2 sm:mt-12 sm:px-4 md:mt-24 md:px-8 lg:mt-32">
         <livewire:links::recent-links/>
     </div>
 </x-app-layout>
