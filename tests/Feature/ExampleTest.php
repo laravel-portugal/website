abca84b6d2297fab175b58a262351e1f24c65682
Tiago Fernandes <tiagoff@gmail.com>
2021-05-07 12:21:26 +0100

    .

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -14,6 +13,7 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
+        $this->withoutExceptionHandling();
         $response = $this->get('/');
 
         $response->assertStatus(200);
