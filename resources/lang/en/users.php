c0755319eab6c5f560ed5b9f5f585786930b08bf
Bruno Gaspar <brunofgaspar1@gmail.com>
2021-10-21 22:00:52 +0100

    fix: Minor typos
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- resources/lang/en/users.php
+++ resources/lang/en/users.php
@@ -2,7 +2,7 @@
 
 return [
     'resource' => 'User',
-    'resource_plural' => 'Users(s)',
+    'resource_plural' => 'User(s)',
 
     'fields' => [
         'id' => 'User ID',
@@ -15,8 +15,8 @@ return [
 
     'delete-title' => 'Delete Account',
     'delete-subtitle' => 'Permanently delete your account on :site_name. All your data will be deleted.',
-    'delete-warning' => 'Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.',
-    'delete-confirmation' => 'Are you sure you want to delete your account? Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.',
+    'delete-warning' => 'Once your account is deleted, all of it\'s resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.',
+    'delete-confirmation' => 'Are you sure you want to delete your account? Once your account is deleted, all of it\'s resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.',
 
     'sessions-title' => 'Browser Sessions',
     'sessions-subtitle' => 'Manage and logout your active sessions on other browsers and devices.',
@@ -28,7 +28,7 @@ return [
     'two-factor-title' => 'Two Factor Authentication',
     'two-factor-subtitle' => 'Add additional security to your account using two factor authentication.',
     'two-factor-on' => 'You have enabled two factor authentication.',
-    'two-factor-on-description' => ' Two factor authentication is now enabled. Scan the following QR code using your phone\'s authenticator application.',
+    'two-factor-on-description' => 'Two factor authentication is now enabled. Scan the following QR code using your phone\'s authenticator application.',
     'two-factor-off' => 'You have not enabled two factor authentication.',
     'two-factor-description' => 'When two factor authentication is enabled, you will be prompted for a secure, random token during authentication. You may retrieve this token from your phone\'s Google Authenticator application or Authy.',
     'two-factor-recovery-codes' => 'Store these recovery codes in a secure password manager. They can be used to recover access to your account if your two factor authentication device is lost.',
