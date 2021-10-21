c0c9c17b8d89bfd9bfe0dcf4727f692d02f326bd
jon <support@indigit.pt>
2021-10-22 00:52:33 +0100

    Fixed Landinh Hero Links

--- resources/js/Pages/Frontend/Landing/Hero.vue
+++ resources/js/Pages/Frontend/Landing/Hero.vue
@@ -18,13 +18,15 @@
         <div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
           <div class="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
             <a
-              href="#"
+              :href="$page.props.meta.social.discord"
+              target="_blank"
+              rel="nofollow"
               class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-base bg-white hover:bg-indigo-50 sm:px-8"
             >
               {{ $t('app.hero-button-one') }}
             </a>
             <a
-              href="#"
+              href="#links"
               class="flex items-center justify-center px-4 py-3 border border-white text-base font-medium rounded-md shadow-sm text-white bg-transparent bg-opacity-60 hover:bg-opacity-70 sm:px-8"
             >
               {{ $t('app.hero-button-two') }}
