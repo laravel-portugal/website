6cffbde8759bf2c66593a5cb7efe91ed6f8334e0
Joao Patricio <ijpatricio@gmail.com>
2023-06-21 19:41:10 +0100

    Remove menu button.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,58 +17,7 @@
                 </div>
             </div>
             <header>
-                <div class="block xl:hidden">
-                    <div class="relative bg-transparent z-50">
-                        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                            <div class="flex justify-between items-center px-4 py-6 sm:px-6">
-                                <div class="flex justify-start lg:w-0 lg:flex-1">
-                                    <a href="/">
-                                 <span class="sr-only">
-                                    Laravel
-                                 </span>
-                                        <div class="flex h-5 w-5 items-center justify-center mx-auto h-20 w-auto">
-                                    <span>
-                                       <svg class="h-8 w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 209">
-                                          <path role="TL" fill="#6da544" d="M1 21l38 22 38-22-38-21z">
-                                          </path>
-                                          <path role="TR" fill="#d80327" d="M124 45l37 21 38-21-38-22z">
-                                          </path>
-                                          <path role="OL" fill="#d80327" d="M0 164l78 45v-43l-41-24v-95l-37-22z">
-                                          </path>
-                                          <path role="OR" fill="#6da544" d="M163 70v44l37-22-.1-43z">
-                                          </path>
-                                          <path role="OB" fill="#6da544" d="M82 166v43l77-45v-43z">
-                                          </path>
-                                          <path role="IL" fill="#6da544" d="M41 47v90l37-22v-90z">
-                                          </path>
-                                          <path role="IR" fill="#ffda44" d="M122 92l37 22v-44l-37-21z">
-                                          </path>
-                                          <path role="IB" fill="#d80327" d="M42.5 140.5l37.5 21.5 77-44.5-37-22.5z">
-                                          </path>
-                                       </svg>
-                                    </span>
-                                            <span class="font-bold ml-2 text-white">
-                                       Laravel.pt
-                                    </span>
-                                        </div>
-                                    </a>
-                                </div>
-                                <div class="flex justify-end -mr-2 -my-2 xl:hidden">
-                                    <button id="headlessui-popover-button-1" type="button" aria-expanded="false" class="bg-transparent rounded-md p-2 inline-flex items-center justify-center text-gray-300 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-500">
-                                 <span class="sr-only">
-                                    Open menu
-                                 </span>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-8 w-8">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7">
-                                            </path>
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
 
-                    </div>
-                </div>
                 <div class="mb-10 hidden xl:block">
                     <div class="relative bg-transparent">
                         <div class="flex justify-between items-center max-w-7xl mx-auto px-4 py-6 sm:px-6 md:justify-start md:space-x-10 lg:px-8">
